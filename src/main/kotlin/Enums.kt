522425bc780f8d7791fa400abe6da7e06ba352cc
AHalic <sophie.gama@aluno.ufes.br>
2022-09-29 23:19:54 -0300

    :refactor: updates enums values

--- src/main/kotlin/Enums.kt
+++ src/main/kotlin/Enums.kt
@@ -1,24 +1,24 @@
-enum class Material(val type:String) {
-    paper("PAPEL"), plastic("PLASTICO"), steel("ACO"), mix("MISTURADO"), others("OUTROS")
+enum class Material() {
+    PAPEL, PLASTICO, ACO, MISTURADO, OUTROS
 }
 
-enum class Rarity(val type:String) {
-    common("COMUM"), uncommon("MEDIO"), rare("RARO"), ultra("RARISSIMO")
+enum class Rarity() {
+    COMUM, MEDIO, RARO, RARISSIMO
 }
 
-enum class TypeCollect(val type:String) {
-    book("LIVRO"), toy("BONECO"), other("OUTROS")
+enum class TypeCollect() {
+    LIVRO, BONECO, OUTROS
 }
 
 enum class TypeElectro(val type:String) {
     video_game("VIDEO-GAMES"), game("JOGOS"), handheld("PORTATEIS"), other("OUTROS")
 }
 
-enum class Size(val type:String) {
-    XS("PP"), S("P"), M("M"), L("G"), XL("GG"), XXL("XG"), XXXL("XXG")
+enum class Size() {
+    PP, P, M, G, GG, XG, XXG
 }
 
-enum class TypeClothes(val type:String) {
-    shirt("CAMISA"), sweatshirt("MOLETON"), accessory("ACESSORIO")
+enum class TypeClothes() {
+    CAMISA, MOLETON, ACESSORIO
 
 }
