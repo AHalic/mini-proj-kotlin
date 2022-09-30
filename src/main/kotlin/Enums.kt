787795266e3ab2c668fd7250a83b575c5ba9a5f7
AHalic <sophie.gama@aluno.ufes.br>
2022-09-30 12:29:43 -0300

    :sparkles: :construction: adds method to get type electro

--- src/main/kotlin/Enums.kt
+++ src/main/kotlin/Enums.kt
@@ -11,7 +11,20 @@ enum class TypeCollect() {
 }
 
 enum class TypeElectro(val type:String) {
-    video_game("VIDEO-GAMES"), game("JOGOS"), handheld("PORTATEIS"), other("OUTROS")
+    video_game("VIDEO-GAMES"), game("JOGOS"), handheld("PORTATEIS"), other("OUTROS");
+
+    /**
+     * Method to return the enum based on its type
+     */
+    @JvmName("valueOf1")
+    fun valueOf(type:String) : TypeElectro {
+        for (value in TypeElectro.values()) {
+            if (type == value.type) {
+                return value
+            }
+        }
+        throw Exception("No electro type matches the given one")
+    }
 }
 
 enum class Size() {
@@ -20,5 +33,4 @@ enum class Size() {
 
 enum class TypeClothes() {
     CAMISA, MOLETON, ACESSORIO
-
 }
