a3786e2bec23d80adf3e5e87dc499654facb10f7
AHalic <sophie.gama@aluno.ufes.br>
2022-09-30 19:41:13 -0300

    :sparkles: :memo: adds function that reads info from compras file and returns a Collectable object

--- src/main/kotlin/Collectable.kt
+++ src/main/kotlin/Collectable.kt
@@ -1,5 +1,25 @@
-class Collectable constructor(val type:TypeCollect, val material:Material, val size:Float, val rarity:Rarity,
+class Collectable constructor(val type:TypeCollect, val material:Material, val size:Float?, val rarity:Rarity,
                             name:String, price_purchase:Float, price_sell:Float, quantity:Int, code:String) :
                             Product(name, price_purchase, price_sell, quantity, code, 'C')  {
 
+    override fun toString(): String {
+        return "$code - Collectable called $name of type $type, size $size, material $material and rarity $rarity\n"
+    }
+}
+
+
+/**
+ * Function to read collectable information of [row] from _compras file_, and generate
+ * a Collectable object, using also products info as: [name], [price_purchase], [price_sell],
+ * [quantity] and [code]
+ *
+ * @return Collectable object
+ */
+fun read_collectable(name:String, price_purchase:Float, price_sell:Float, quantity:Int, code:String, row:Map<String, String>) : Collectable {
+    val type = row["Tipo"].let { if (it?.checkHyphen() != null) TypeCollect.valueOf(it!!.unaccentUpper()) else throw Exception("Collectable type cant be null")}
+    val material = row["Material de fabricação"].let { if (it?.checkHyphen() != null) Material.valueOf(it!!.unaccentUpper()) else throw Exception("Collectable material cant be null")}
+    val size = row["Tamanho"].let { if (it?.checkHyphen() != null) it!!.toFloat() else null}
+    val rarity = row["Relevância"].let { if (it?.checkHyphen() != null) Rarity.valueOf(it!!.unaccentUpper()) else throw Exception("Collectable rarity cant be null")}
+
+    return Collectable(type, material, size, rarity, name, price_purchase, price_sell, quantity, code)
 }
