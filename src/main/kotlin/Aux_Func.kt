9716b1ab5ce8888690fffe2d71df5e9741a93bcf
AHalic <sophie.gama@aluno.ufes.br>
2022-10-01 18:10:44 -0300

    :sparkles: adds float format

--- src/main/kotlin/Aux_Func.kt
+++ src/main/kotlin/Aux_Func.kt
@@ -1,4 +1,7 @@
+import java.text.DecimalFormat
 import java.text.Normalizer
+import java.text.NumberFormat
+import java.util.*
 
 private val REGEX_UNACCENT = "\\p{InCombiningDiacriticalMarks}+".toRegex()
 
@@ -21,4 +24,28 @@ fun CharSequence.unaccentUpper(): String {
  */
 fun String.checkHyphen() : String? {
     return if (this != "-") this else null
+}
+
+/**
+ * Auxiliary function to format float [value] according to the following rules:
+ * - minimum 0 fraction digits
+ * - maximum 2 fraction digits
+ * - no group separator
+ * - dot ('.') as decimal separator
+ *
+ *      __example__: 10250.3
+ *
+ * @return String
+ */
+fun formatDecimal(value: Float) : String {
+    val numberFormat = NumberFormat.getNumberInstance(Locale.US)
+    numberFormat.maximumFractionDigits = 2
+    numberFormat.minimumFractionDigits = 0
+
+    if (numberFormat is DecimalFormat) {
+        numberFormat.isDecimalSeparatorAlwaysShown = false
+        numberFormat.isGroupingUsed = false
+    }
+
+    return numberFormat.format(value)
 }
