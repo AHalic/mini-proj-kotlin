7ba62b46b6c50e81f5387c2b5c15c3901eddb4a4
AHalic <sophie.gama@aluno.ufes.br>
2022-10-01 01:17:29 -0300

    :recycle: removes private

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -1,6 +1,6 @@
 open class Product constructor (var name:String, var price_purchase:Float, var price_sell:Float, var quantity:Int) {
     var code:String? = null
-    private var category:Char? = null
+    var category:Char? = null
 
     constructor(name:String, price_purchase:Float, price_sell:Float, quantity:Int, code:String, category:Char): this(name, price_purchase, price_sell, quantity) {
         this.category = category
